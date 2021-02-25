41562f73468b024a2457209e83d9c8aa870e11d9
Mark Francisco <mfrancisco581@gmail.com>
2021-02-25 14:13:31 -0600

    Add logic to print message if name starts with A

--- app/controllers/api/params_game_app_controller.rb
+++ app/controllers/api/params_game_app_controller.rb
@@ -2,6 +2,10 @@ class Api::ParamsGameAppController < ApplicationController
 
   def display_name
     @name_to_render = params["your_name"].upcase
+    if @name_to_render[0] == "A"
+      @message_to_render = "starts with A"   
+    end
+    
     render "params_name_view.json.jb"
   end
 
