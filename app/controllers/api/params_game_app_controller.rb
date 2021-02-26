35f1902a5cd25b3fca6911cef86e43ef2b6859b1
Mark Francisco <mfrancisco581@gmail.com>
2021-02-26 13:51:13 -0600

    Add parameters to GET requrest routes in browser

--- app/controllers/api/params_game_app_controller.rb
+++ app/controllers/api/params_game_app_controller.rb
@@ -9,9 +9,9 @@ class Api::ParamsGameAppController < ApplicationController
     render "params_name_view.json.jb"
   end
 
-  def display_user_guess
-    @instructions = "guess a number from 0 to 100. your guess goes into the 'guess' param in your browser."
-    @guess_to_render = params["guess"].to_i
+  def display_guess
+    @instructions = "guess a number from 0 to 100. your guess goes into the 'guess' QUERY STRING param in your browser."
+    @guess_to_render = params[:guess].to_i
     if @guess_to_render == 36
       @hint = "Correct!"
     elsif @guess_to_render > 36 && @guess_to_render <= 100
@@ -24,4 +24,19 @@ class Api::ParamsGameAppController < ApplicationController
     render "guessing_game_view.json.jb"
   end  
 
+  def display_guess_url_segment
+    @instructions = "guess a number from 0 to 100. your guess goes into the 'guess' URL SEGMENT param in your browser."
+    @guess_to_render = params[:guess_url_segment].to_i
+    if @guess_to_render == 36
+      @hint = "NICE JOB!!!!!!!!!!!!"
+    elsif @guess_to_render > 36 && @guess_to_render <= 100
+      @hint = "TOO HIGH!"
+    elsif @guess_to_render < 36 && @guess_to_render >= 0
+      @hint = "TOO LOW!"
+    else
+      @hint = "your guess needs to be between 0 and 100."
+    end
+    
+    render "guessing_game_view.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     get "/name" => "params_game_app#display_name"
 
-    get "/guessing_game" => "params_game_app#display_user_guess"
+    get "/guessing_game" => "params_game_app#display_guess"
+
+    # wildcard KEY gets defined in route.
+    # wildcard VALUE gets defined in browser.
+    get "/guessing_game_url_segments/:guess_url_segment" => "params_game_app#display_guess_url_segment"
   end
 end
